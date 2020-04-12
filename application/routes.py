be7df9a046ed35aad7dfd466440caf1d3eba2936
brandon wallace <17948072+brandon-wallace@users.noreplay.github.com>
2020-04-12 15:13:46 -0400

    Improve application

--- application/routes.py
+++ application/routes.py
@@ -1,13 +1,12 @@
 import json
 import requests
 from geopy.geocoders import Nominatim
-from geopy.exc import GeocoderTimedOut
+from geopy.exc import GeocoderTimedOut, GeocoderServiceError
 from datetime import datetime
 from flask import render_template, request, redirect, url_for
 from flask_babel import format_datetime
 from application import app, babel, api_key
 from application.forms import AddressForm
-from application.json_data import weather_report
 
 lon, lat = None, None
 
@@ -20,105 +19,114 @@ def get_locale():
 
 
 def find_location(addr):
-    '''Get latitude/longitude'''
+    '''Get latitude and longitude'''
 
     try:
         return geolocator.geocode(addr)
-    except GeocoderTimedOut as e:
+    except (GeocoderTimedOut, GeocoderServiceError) as e:
         print(e)
+        return
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     '''Index route'''
 
-    date_time = format_datetime(datetime.now())
     form = AddressForm()
-    content = {
-            'date_time': date_time,
-            'form': form
-            }
-    return render_template('index.html', **content)
+    return render_template('index.html', form=form)
 
 
 @app.route('/weather', methods=['POST'])
 def get_local_weather():
-    '''Display local weather report based of geolocation'''
+    '''Display local weather report based on geolocation'''
 
     form = AddressForm()
     addr = request.form['address']
     geolocator = Nominatim(user_agent='application', timeout=3)
     location = geolocator.geocode(addr)
     if location is None:
-        return render_template('index.html', message='Location not found')
+        return render_template(index.html, message="Location Not Found")
     else:
         lat = location.latitude
         lon = location.longitude
         local_address = location.address
         url = requests.get('https://api.darksky.net/forecast/{}/{},{}'.format(api_key, lat, lon))
-        # if url.response == 200:
-        report = url.text
-        data = json.loads(report)
-        json.dumps(data, ensure_ascii=False)
-        temps = []
-        temps_celcius = []
-        hours = []
-        forecast = []
-        humidity = []
-        wind_speed = []
-        visibility = []
-        pressure = []
-        uv_index = []
-        ozone = []
-        date_time = datetime.now().strftime('%c')
+        if url.status_code == 200:
+            report = url.text
+            data = json.loads(report)
+            json.dumps(data, ensure_ascii=False)
+            temps = []
+            temps_celcius = []
+            hours = []
+            forecast = []
+            humidity = []
+            wind_speed = []
+            visibility = []
+            pressure = []
+            uv_index = []
+            ozone = []
+            date_time = datetime.now().strftime('%c')
 
-        current_temp = data['currently']['temperature']
-        current_temp_c = (current_temp - 32) * 5.0 / 9.0
-        current_forecast = data['currently']['summary']
-        timezone = data['timezone']
-        current_low = data['daily']['data'][0]['temperatureMin']
-        current_low_c = (current_low - 32) * 5.0 / 9.0
-        current_high = data['daily']['data'][0]['temperatureMax']
-        current_high_c = (current_high - 32) * 5.0 / 9.0
-        sunrise = datetime.fromtimestamp(data['daily']['data'][0]['sunriseTime']).strftime('%Hh:%Mm')
-        sunset = datetime.fromtimestamp(data['daily']['data'][0]['sunsetTime']).strftime('%Hh:%Mm')
+            current_temp = data['currently']['temperature']
+            current_temp_c = (current_temp - 32) * 5.0 / 9.0
+            current_forecast = data['currently']['summary']
+            timezone = data['timezone']
+            current_low = data['daily']['data'][0]['temperatureLow']
+            current_low_c = (current_low - 32) * 5.0 / 9.0
+            current_high = data['daily']['data'][0]['temperatureHigh']
+            current_high_c = (current_high - 32) * 5.0 / 9.0
+            sunrise = datetime.fromtimestamp(data['daily']['data'][0]['sunriseTime']).strftime('%Hh:%Mm')
+            sunset = datetime.fromtimestamp(data['daily']['data'][0]['sunsetTime']).strftime('%Hh:%Mm')
 
-        for txt in data['hourly']['data']:
-            hours.append(datetime.fromtimestamp(txt['time']).strftime("%H"))
-            temps.append(txt['temperature'])
-            temps_celcius.append((int(txt['temperature']) - 32) * 5.0 / 9.0)
-            forecast.append(txt['summary'])
-            humidity.append(txt['humidity'])
-            wind_speed.append(txt['windSpeed'])
-            uv_index.append(txt['uvIndex'])
-            ozone.append(txt['ozone'])
-            visibility.append(txt['visibility'])
-            pressure.append(txt['pressure'])
+            for txt in data['hourly']['data']:
+                hours.append(datetime.fromtimestamp(txt['time']).strftime("%H"))
+                temps.append(txt['temperature'])
+                temps_celcius.append((int(txt['temperature']) - 32) * 5.0 / 9.0)
+                forecast.append(txt['summary'])
+                humidity.append(txt['humidity'])
+                wind_speed.append(txt['windSpeed'])
+                uv_index.append(txt['uvIndex'])
+                ozone.append(txt['ozone'])
+                visibility.append(txt['visibility'])
+                pressure.append(txt['pressure'])
 
-            content = {
-                    'sunrise': sunrise,
-                    'sunset': sunset,
-                    'local_address': local_address,
-                    'lat': lat,
-                    'lon': lon,
-                    'current_temp': current_temp,
-                    'current_temp_c': current_temp_c,
-                    'current_forecast': current_forecast,
-                    'timezone': timezone,
-                    'current_low': current_low,
-                    'current_low_c': current_low_c,
-                    'current_high': current_high,
-                    'current_high_c': current_high_c,
-                    'date_time': date_time,
-                    'hours': hours,
-                    'temps': temps,
-                    'temps_celcius': temps_celcius,
-                    'forecast': forecast,
-                    'humidity': humidity,
-                    'wind_speed': wind_speed,
-                    'uv_index': uv_index,
-                    'ozone': ozone,
-                    'visibility': visibility,
-                    'pressure': pressure
-                    }
+            daily_high = []
+            daily_low = []
+            daily_datetime = []
+            for i in range(7):
+                daily_high.append(data['daily']['data'][i]['temperatureHigh'])
+                daily_low.append(data['daily']['data'][i]['temperatureLow'])
+                daily_datetime.append(datetime.fromtimestamp(data['daily']['data'][i]['time']).strftime('%b %d'))
+
+                content = {
+                        'daily_high': daily_high,
+                        'daily_low': daily_low,
+                        'daily_datetime': daily_datetime,
+                        'sunrise': sunrise,
+                        'sunset': sunset,
+                        'local_address': local_address,
+                        'lat': lat,
+                        'lon': lon,
+                        'current_temp': current_temp,
+                        'current_temp_c': current_temp_c,
+                        'current_forecast': current_forecast,
+                        'timezone': timezone,
+                        'current_low': current_low,
+                        'current_low_c': current_low_c,
+                        'current_high': current_high,
+                        'current_high_c': current_high_c,
+                        'date_time': date_time,
+                        'hours': hours,
+                        'temps': temps,
+                        'temps_celcius': temps_celcius,
+                        'forecast': forecast,
+                        'humidity': humidity,
+                        'wind_speed': wind_speed,
+                        'uv_index': uv_index,
+                        'ozone': ozone,
+                        'visibility': visibility,
+                        'pressure': pressure
+                        }
+            else:
+                content = {}
     return render_template('weather.html', form=form, **content)
