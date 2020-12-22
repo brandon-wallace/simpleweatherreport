9667276268b32f467f9f7dedf8c7fd1b489b3581
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2020-12-21 20:46:41 -0500

    Import key

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,4 @@
-import os
+from os import environ
 from flask import Flask
 from flask_babel import Babel
 
@@ -10,8 +10,9 @@ def create_app():
     '''Initialize application'''
 
     app.config['DEBUG'] = False
-    app.config['FLASK_ENV'] = os.getenv('FLASK_ENV')
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['FLASK_ENV'] = environ.get('FLASK_ENV')
+    app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
+    app.config['OWM_API_KEY'] = environ.get('OWM_API_KEY')
     app.config['BABEL_DEFAULT_LOCALE'] = 'en'
 
     babel.init_app(app)
