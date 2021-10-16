bc4a4a2a75936d079f410531368a5967a9dc39b8
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-10-16 17:29:40 -0400

    Test index page
    Added index page to test for a 200 response.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,3 +1,4 @@
+from application import create_app
 from application.routes import fahrenheit_to_celcius
 
 
@@ -5,3 +6,13 @@ def test_fahrenheit_to_celcius():
     '''Test conversion to celcius'''
 
     assert fahrenheit_to_celcius(43) == (43 - 32) / 1.8
+
+
+def test_index_page():
+    '''Test index page route'''
+
+    app = create_app()
+
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
