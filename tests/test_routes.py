a3233ed00bb01169933a3167f392159bf0b02030
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2022-04-10 19:46:17 -0400

    fix: Fix pytest

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,5 +1,17 @@
+import pytest
 from application import create_app
 from application.routes import fahrenheit_to_celcius
+
+app = create_app()
+
+
+@pytest.fixture()
+def client():
+    '''Client for testing routes'''
+
+    with app.test_client() as client:
+
+        yield client
 
 
 def test_fahrenheit_to_celcius():
@@ -8,11 +20,8 @@ def test_fahrenheit_to_celcius():
     assert fahrenheit_to_celcius(43) == (43 - 32) / 1.8
 
 
-def test_index_page():
+def test_index_page(client):
     '''Test index page route'''
 
-    app = create_app()
-
-    with app.test_client() as client:
-        response = client.get('/')
-        assert response.status_code == 200
+    response = client.get('/')
+    assert response.status_code == 200
